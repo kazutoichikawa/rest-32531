caf41259a19091ce89353fcb437576633ed379f3
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-31 19:24:56 +0900

    p_numカラムを追加

--- db/migrate/20201226074539_create_shops.rb
+++ db/migrate/20201226074539_create_shops.rb
@@ -4,6 +4,7 @@ class CreateShops < ActiveRecord::Migration[6.0]
       t.string :shop_name,      null: false
       t.integer :genre_id,      null: false
       t.text :profile,          null: false
+      t.string :p_num,            null: false
       t.string :p_code,         null: false
       t.integer :prefecture_id, null: false
       t.string :city,           null: false
